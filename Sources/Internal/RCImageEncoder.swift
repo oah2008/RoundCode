5f7e0a0045979f153ff98759ba654068ee423b46
omar hariri <hariri75@icloud.com>
2021-06-21 20:05:23 +0300

    Update RCImageEncoder.swift

--- Sources/Internal/RCImageEncoder.swift
+++ Sources/Internal/RCImageEncoder.swift
@@ -35,7 +35,7 @@ struct RCImageEncoder {
     let renderer = UIGraphicsImageRenderer(bounds: rect, format: .default())
     let renderedImage = renderer.image { context in
       let cgContext = context.cgContext
-        #colorLiteral(red: 0.9490196078, green: 0.9333333333, blue: 0.8901960784, alpha: 1).setFill()
+        #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1).setFill()
         context.fill(rect)
       cgContext.translateBy(x: image.contentInsets.left, y: image.contentInsets.top)
       cgContext.saveGState()
